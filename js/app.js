e94c99f9aa1855eea9f2c0548af9d74995985fc6
educalo <64586573+educalo@users.noreply.github.com>
2024-03-15 18:43:03 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    mostrarAuto(autos);
+    buscarAutos();
+});
+
+let resultado = document.getElementById("resultado"),
+    search = document.getElementById("search");
+
+function mostrarAuto(autos){
+    autos.forEach(auto => {
+        const elements = document.createElement("P");
+        elements.innerHTML = `Marca: <strong>${auto.marca}</strong> - Modelo: <strong>${auto.modelo}</strong> - Año: <strong>${auto.year}</strong> - Precio: <strong>${auto.precio}</strong> - Color: <strong>${auto.color}</strong>`;
+        resultado.appendChild(elements);
+    });  
+}
+
+function buscarAutos(){
+    search.addEventListener("input", e=>{
+        limpiarHTML();
+        const inputText = e.target.value.toUpperCase().trim();
+        
+        const mostrarFiltrado = autos.filter(auto => 
+            auto.marca.toUpperCase().startsWith(inputText) || 
+            auto.modelo.toUpperCase().startsWith(inputText) ||
+            auto.year.toString().startsWith(inputText) ||
+            auto.precio.toString().startsWith(inputText) ||
+            auto.color.toUpperCase().startsWith(inputText) 
+        );
+
+        if (mostrarFiltrado.length === 0) {
+            noResultado();
+        }
+        
+        mostrarAuto(mostrarFiltrado);
+    });
+}
+
+function limpiarHTML(){
+    while (resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+}
+
+function noResultado(){
+    const noResultadoElement = document.createElement("DIV");
+    noResultadoElement.textContent = "No hay resultados de búsqueda";
+    resultado.appendChild(noResultadoElement);    
+}
